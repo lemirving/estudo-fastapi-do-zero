5fb6ebbd7dcb6538702ceb5577d36dbbfb84a465
Irving Lemuel <dev.lemirving@gmail.com>
2026-08-12 21:11:47 -0400

    Implementando autorização nos endpoints

--- fastapi_zero/app.py
+++ fastapi_zero/app.py
@@ -2,6 +2,7 @@ from http import HTTPStatus
 
 from fastapi import FastAPI, HTTPException
 from fastapi.params import Depends
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy import select
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import Session
@@ -10,9 +11,16 @@ from fastapi_zero.database import get_session
 from fastapi_zero.models import User
 from fastapi_zero.schemas import (
     Message,
+    Token,
     UserList,
     UserPublic,
     UserSchema,
+)
+from fastapi_zero.security import (
+    create_access_token,
+    get_current_user,
+    get_password_hash,
+    verify_password,
 )
 
 app = FastAPI(title="Irving API")
@@ -45,7 +53,9 @@ def create_user(user: UserSchema, session=Depends(get_session)):
             )
     # Se não houver erro
     db_user = User(
-        username=user.username, email=user.email, password=user.password
+        username=user.username,
+        email=user.email,
+        password=get_password_hash(user.password),
     )
     session.add(db_user)
     session.commit()
@@ -56,7 +66,10 @@ def create_user(user: UserSchema, session=Depends(get_session)):
 
 @app.get("/users/", status_code=HTTPStatus.OK, response_model=UserList)
 def read_users(
-    limit: int = 10, offset: int = 0, session: Session = Depends(get_session)
+    limit: int = 10,
+    offset: int = 0,
+    session: Session = Depends(get_session),
+    current_user=Depends(get_current_user),
 ):
     users = session.scalars(select(User).limit(limit).offset(offset))
     return {"users": users}
@@ -66,24 +79,28 @@ def read_users(
     "/users/{user_id}", status_code=HTTPStatus.OK, response_model=UserPublic
 )
 def update_user(
-    user_id: int, user: UserSchema, session: Session = Depends(get_session)
+    user_id: int,
+    user: UserSchema,
+    session: Session = Depends(get_session),
+    current_user: User = Depends(get_current_user),
 ):
-    user_db = session.scalar(select(User).where(User.id == user_id))
 
-    if not user_db:
-        raise HTTPException(
-            status_code=HTTPStatus.NOT_FOUND, detail="User Not Found"
-        )
+    credentials_exception = HTTPException(
+        status_code=HTTPStatus.FORBIDDEN,
+        detail="Not Enough permissions",
+    )
+    current_user.email = user.email
+    current_user.username = user.username
+    current_user.password = get_password_hash(user.password)
 
-    user_db.email = user.email
-    user_db.username = user.username
-    user_db.password = user.password
+    if not current_user.id == user_id:
+        raise credentials_exception
     try:
-        session.add(user_db)
+        session.add(current_user)
         session.commit()
-        session.refresh(user_db)
+        session.refresh(current_user)
 
-        return user_db
+        return current_user
     except IntegrityError:
         raise HTTPException(
             status_code=HTTPStatus.CONFLICT,
@@ -94,15 +111,40 @@ def update_user(
 @app.delete(
     "/users/{user_id}", status_code=HTTPStatus.OK, response_model=Message
 )
-def delete_user(user_id: int, session: Session = Depends(get_session)):
-    user_db = session.scalar(select(User).where(User.id == user_id))
+def delete_user(
+    user_id: int,
+    session: Session = Depends(get_session),
+    current_user: User = Depends(get_current_user),
+):
 
-    if not user_db:
-        raise HTTPException(
-            status_code=HTTPStatus.NOT_FOUND, detail="User Not Found"
-        )
+    credentials_exception = HTTPException(
+        status_code=HTTPStatus.FORBIDDEN,
+        detail="Not Enough permissions",
+    )
 
-    session.delete(user_db)
+    if not current_user.id == user_id:
+        raise credentials_exception
+    session.delete(current_user)
     session.commit()
 
     return {"message": "User Deleted"}
+
+
+@app.post("/login", response_model=Token)
+def login_for_access_token(
+    form_data: OAuth2PasswordRequestForm = Depends(),
+    session: Session = Depends(get_session),
+):
+    user = session.scalar(select(User).where(User.email == form_data.username))
+
+    if not user:
+        raise HTTPException(
+            status_code=HTTPStatus.UNAUTHORIZED,
+            detail="Incorret email or password",
+        )
+    if not verify_password(form_data.password, user.password):
+        raise HTTPException(
+            status_code=HTTPStatus.UNAUTHORIZED, detail="Incorret password"
+        )
+    access_token = create_access_token({"sub": user.email})
+    return {"access_token": access_token, "token_type": "Bearer"}
--- tests/test_app.py
+++ tests/test_app.py
@@ -35,24 +35,20 @@ def test_create_user(client):
     }
 
 
-def test_read_users(client):
-    response = client.get("/users/")
-
-    assert response.status_code == HTTPStatus.OK
-    assert response.json() == {"users": []}
-
-
-def test_read_users_with_users(client, user):
+def test_read_users(client, user, token):
     user_schema = UserPublic.model_validate(user).model_dump()
-    response = client.get("/users/")
+    response = client.get(
+        "/users/", headers={"Authorization": f"Bearer {token}"}
+    )
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {"users": [user_schema]}
 
 
-def test_update_user(client, user):
+def test_update_user(client, user, token):
     response = client.put(
         "/users/1",
+        headers={"Authorization": f"Bearer {token}"},
         json={
             "username": "bob",
             "email": "bob@example.com",
@@ -68,33 +64,48 @@ def test_update_user(client, user):
     }
 
 
-def test_update_integrity_error(client, user):
+def test_update_integrity_error(client, user, token):
     # Inserindo fausto
     client.post(
-        '/users',
+        "/users",
+        headers={"Authorization": f"Bearer {token}"},
         json={
-            'username': 'fausto',
-            'email': 'fausto@example.com',
-            'password': 'secret',
+            "username": "fausto",
+            "email": "fausto@example.com",
+            "password": "secret",
         },
     )
 
     # Alterando o user das fixture para fausto
     response = client.put(
-        f'/users/{user.id}',
+        f"/users/{user.id}",
+        headers={"Authorization": f"Bearer {token}"},
         json={
-            'username': 'fausto',
-            'email': 'bob@example.com',
-            'password': 'mynewpassword',
+            "username": "fausto",
+            "email": "bob@example.com",
+            "password": "mynewpassword",
         },
     )
 
     assert response.status_code == HTTPStatus.CONFLICT
-    assert response.json() == {'detail': "Username or email already exists"}
+    assert response.json() == {"detail": "Username or email already exists"}
 
 
-def test_delete_user(client, user):
-    response = client.delete("users/1")
+def test_delete_user(client, user, token):
+    response = client.delete(
+        f"users/{user.id}", headers={"Authorization": f"Bearer {token}"}
+    )
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {"message": "User Deleted"}
+
+
+def test_get_token(client, user):
+    response = client.post(
+        "/login",
+        data={"username": user.email, "password": user.clean_password},
+    )
+    token = response.json()
+    assert response.status_code == HTTPStatus.OK
+    assert token["token_type"] == "Bearer"
+    assert "access_token" in token
